8a203dce00627cf9f8b19e07f3aacac30be3c9db
khoaleee <Letrananhkhoa09042005@gmail.com>
2026-02-25 15:24:45 +0700

    Init project with frontend and backend

---
+++ backend/src/main/java/com/example/project_group5/ProjectGroup5Application.java
@@ -0,0 +1,13 @@
+package com.example.project_group5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGroup5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectGroup5Application.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/project_group5/ProjectGroup5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_group5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGroup5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
