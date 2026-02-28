b5ce7ff61ab5e424a48b32e1795721dd419c8064
Lê Trần Anh Khoa <letrananhkhoa09042005@gmail.com>
2026-02-28 16:19:43 +0000

    Revert "Merge branch 'feature/entity' into 'main'"
    This reverts merge request !3

--- backend/src/main/java/repository/CustomerFranchiesRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import entity.CustomerFranchies;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerFranchiesRepository extends JpaRepository<CustomerFranchies,Long> {
-}
