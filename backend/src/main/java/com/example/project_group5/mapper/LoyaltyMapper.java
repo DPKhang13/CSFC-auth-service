8a414e35e128848a8225dc4eca3b224457d26e22
khoaleee <Letrananhkhoa09042005@gmail.com>
2026-03-01 23:35:44 +0700

    [FIX]Add swagger

---
+++ backend/src/main/java/com/example/project_group5/mapper/LoyaltyMapper.java
@@ -0,0 +1,4 @@
+package com.example.project_group5.mapper;
+
+public class LoyaltyMapper {
+}
--- backend/src/main/java/com/example/project_group5/service/serviceImpl/LoyaltyService.java
+++
@@ -1,5 +0,0 @@
-package com.example.project_group5.service.serviceImpl;
-
-
-public class LoyaltyService {
-}
