d31d678544d8fdd574e79d370dee300a8440a078
Điền Trần <dientran0410@gmail.com>
2026-03-02 22:11:26 +0700

    [FEAT]Thêm chức năng áp mã giảm giá

--- engagement/src/main/java/com/group5/engagement/controller/customer/CouponController.java
+++ engagement/src/main/java/com/group5/engagement/controller/customer/CouponController.java
@@ -1,4 +1,19 @@
 package com.group5.engagement.controller.customer;
 
+import com.group5.engagement.dto.request.ApplyCouponRequest;
+import com.group5.engagement.dto.response.ApplyCouponResponse;
+import com.group5.engagement.service.CouponService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/coupons")
 public class CouponController {
+    @Autowired
+    private CouponService couponService;
+
+    @PostMapping("/apply")
+    public ApplyCouponResponse apply(@RequestBody ApplyCouponRequest req){
+        return couponService.applyCoupon(req);
+    }
 }
