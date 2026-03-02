3930570f50eee7597e7161493cc95028883940f3
Điền Trần <dientran0410@gmail.com>
2026-03-02 22:42:18 +0700

    [FEAT] Thêm Exception cho coupon

--- engagement/src/main/java/com/group5/engagement/service/impl/CouponServiceImpl.java
+++ engagement/src/main/java/com/group5/engagement/service/impl/CouponServiceImpl.java
@@ -3,6 +3,7 @@ package com.group5.engagement.service.impl;
 import com.group5.engagement.dto.request.ApplyCouponRequest;
 import com.group5.engagement.dto.response.ApplyCouponResponse;
 import com.group5.engagement.entity.Coupon;
+import com.group5.engagement.exception.coupon.CouponNotFoundException;
 import com.group5.engagement.repository.CouponRepository;
 import com.group5.engagement.service.CouponService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,10 +19,14 @@ public class CouponServiceImpl implements CouponService {
     public ApplyCouponResponse applyCoupon(ApplyCouponRequest req) {
         Coupon coupon = couponRepository
                 .findByCode(req.getCouponCode())
-                .orElseThrow(() -> new RuntimeException("Coupon not found"));
+                .orElseThrow(CouponNotFoundException::new);
 
         if (coupon.getUsedCount() >= coupon.getUsageLimit()) {
-            throw new RuntimeException("Coupon hết lượt dùng");
+            throw new RuntimeException("Phiếu giảm giá hết lượt dùng");
+        }
+
+        if (req.getOrderAmount() < coupon.getMinOrderValue()) {
+            throw new RuntimeException("Tổng số tiền đơn hàng không đủ để áp dụng phiếu giảm giá");
         }
 
         double discount = calculateDiscount(coupon, req.getOrderAmount());
@@ -43,7 +48,13 @@ public class CouponServiceImpl implements CouponService {
 
         if ("PERCENTAGE".equals(coupon.getDiscountType())) {
 
-            return amount * coupon.getDiscountValue() / 100;
+            double discount = amount * coupon.getDiscountValue() / 100;
+
+            if (coupon.getMaxDiscount() != 0) {
+                discount = Math.min(discount, coupon.getMaxDiscount());
+            }
+
+            return discount;
         }
 
         return 0;
