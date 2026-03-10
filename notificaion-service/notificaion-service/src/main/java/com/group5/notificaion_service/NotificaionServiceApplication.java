c289145e26278ecce28eca03dedb93ba61d61911
minhfpt2004 <minhtkse181869@fpt.edu.vn>
2026-03-10 11:01:11 +0700

    [INIT] tạo source code notification service

---
+++ notificaion-service/notificaion-service/src/main/java/com/group5/notificaion_service/NotificaionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.group5.notificaion_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificaionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificaionServiceApplication.class, args);
+	}
+
+}
---
+++ notificaion-service/notificaion-service/src/test/java/com/group5/notificaion_service/NotificaionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group5.notificaion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificaionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
